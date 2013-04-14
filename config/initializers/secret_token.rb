1f3abfc1964f1d0d4e1fe10a578d6bf97270b088
Amit Kumar Gupta <amitkgupta84@gmail.com>
2013-04-14 13:02:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpotMarket::Application.config.secret_token = 'd929bbd1974c7653a1acd0fb26e84253598c5f6004c16c21dd2dfba9b7d11e67e28ee1f480f4496b5d157e154399f9f9d476605bc5b8d2931cb70d19f4ae0588'
