db315071fe8762cf91614bdeeee738324bce7d7b
Amit Kumar Gupta <amitkgupta84@gmail.com>
2013-05-19 05:05:36 -0700

    quick and dirty addition of all instance types and additional product description
    TODO: refactor this so it's done in a sane way

--- app/controllers/spot_prices_controller.rb
+++ app/controllers/spot_prices_controller.rb
@@ -1,8 +1,39 @@
 class SpotPricesController < ApplicationController
   def index
-    @spot_prices = SpotPrice.where(instance_type: "cr1.8xlarge", product_description: "Linux/UNIX (Amazon VPC)").group_by(&:availability_zone)
-    @spot_prices.each do |az, spot_price_data|
-    	@spot_prices[az] = spot_price_data.map { |sp| [1000*sp.timestamp.to_i, sp.spot_price.to_f] }.sort_by { |x| x[0] } 
+  	instance_types = [
+      "m1.small",
+      "m1.medium",
+      "m1.large",
+      "m1.xlarge",
+      "m3.xlarge",
+      "m3.2xlarge",
+      "c1.medium",
+      "c1.xlarge",
+      "cc2.8xlarge",
+	  "m2.xlarge",
+      "m2.2xlarge",
+      "m2.4xlarge",
+      "cr1.8xlarge",
+      "hi1.4xlarge",
+      "hs1.8xlarge",
+      "t1.micro",
+      "cg1.4xlarge"
+    ]
+    product_descriptions = [
+      "Linux/UNIX (Amazon VPC)",
+      "Linux/UNIX"
+    ]
+
+    @spot_prices = Hash.new
+
+    instance_types.each do |instance_type|
+      product_descriptions.each do |product_description|
+    	instance_spot_prices = SpotPrice.where(instance_type: instance_type, product_description: product_description).group_by(&:availability_zone)
+    	instance_spot_prices.each do |az, spot_price_data|
+    	  instance_spot_prices[az] = spot_price_data.map { |sp| [1000*sp.timestamp.to_i, sp.spot_price.to_f] }.sort_by { |x| x[0] }
+    	end
+    	@spot_prices[[instance_type, product_description]] = instance_spot_prices
+      end
     end
   end
 end
--- lib/tasks/spot_price.rake
+++ lib/tasks/spot_price.rake
@@ -2,10 +2,39 @@ namespace :spot_price do
   desc "Fetch and save recent spot price records from AWS"
   task fetch_recent: :environment do
 	aws_client = AWS::EC2::Client.new
-	aws_hi_mem_vpc_spot_prices = aws_client.describe_spot_price_history({
-		instance_types: ["cr1.8xlarge"], 
-		product_descriptions: ["Linux/UNIX (Amazon VPC)"]
-	})
-    SpotPrice.create aws_hi_mem_vpc_spot_prices[:spot_price_history_set]
+    instance_types = [
+      "m1.small",
+      "m1.medium",
+      "m1.large",
+      "m1.xlarge",
+      "m3.xlarge",
+      "m3.2xlarge",
+      "c1.medium",
+      "c1.xlarge",
+      "cc2.8xlarge",
+      "m2.xlarge",
+      "m2.2xlarge",
+      "m2.4xlarge",
+      "cr1.8xlarge",
+      "hi1.4xlarge",
+      "hs1.8xlarge",
+      "t1.micro",
+      "cg1.4xlarge"
+    ]
+    product_descriptions = [
+      "Linux/UNIX (Amazon VPC)",
+      "Linux/UNIX"
+    ]
+
+    instance_types.each do |instance_type|
+      product_descriptions.each do |product_description|
+		SpotPrice.create(
+    	  aws_client.describe_spot_price_history({
+			instance_types: [instance_type],
+			product_descriptions: [product_description]
+		  })[:spot_price_history_set]
+		)
+	  end
+	end
   end
 end
